87c5f12267face7d3715efaa421b227e014f4bf3
şeyma bayram <seymabayram@seyma-MacBook-Air.local>
2026-04-09 17:52:26 +0300

    UI: Replaced native date pickers with Flatpickr (English) and set global culture to en-US.

--- Program.cs
+++ Program.cs
@@ -41,6 +41,15 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+// Set default culture to English (en-US)
+var supportedCultures = new[] { "en-US" };
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
+app.UseRequestLocalization(localizationOptions);
+
 app.UseAuthentication();
 app.UseAuthorization();
 
